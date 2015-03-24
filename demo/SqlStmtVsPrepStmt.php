c82596fd1281edfd1dbe839eb8f12fae7985321f
Chris Shumake <chris.shumake@bronto.com>
2015-03-24 13:10:00 -0400

    Adding in configuration example file and first demo file

--- demo/SqlStmtVsPrepStmt.php
+++ demo/SqlStmtVsPrepStmt.php
@@ -7,9 +7,53 @@ if (basename($here) != 'CustomDB') {
 }
 require_once('cli.php');
 
+// Note that this script does not use the CustomDB methods but, instead, the PDO methods. As a demo, it is more useful to use PDO directly.
 
-$dbHandle = CustomDB::getDBH();
+// SQL Statement Code
+$pdoHandle = CustomDB::getDBH();
 $sql = 'SELECT ID, Name FROM city LIMIT 3';
-$PdoStatement = $dbHandle->query($sql);
-print_r($PdoStatement->fetchAll());
+$pdoStatement = $pdoHandle->query($sql);
+$pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+//print_r($pdoStatement->fetchAll());
 
+// Prep Stmt Code
+$pdoHandle = CustomDB::getDBH();
+$sql = 'SELECT ID, Name FROM city LIMIT 3';
+$pdoStatement = $pdoHandle->prepare($sql);
+$pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+$pdoStatement->execute();
+//print_r($pdoStatement->fetchAll());
+
+// SQL Statement Code w/ Parameter
+$pdoHandle = CustomDB::getDBH();
+$sql = 'SELECT ID, Name FROM city WHERE Name = ' . $pdoHandle->quote('Kabul') . ' LIMIT 3';
+$pdoStatement = $pdoHandle->query($sql);
+$pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+print_r($pdoStatement->fetchAll());
+
+// Prep Stmt Code w/ Parameter
+$pdoHandle = CustomDB::getDBH();
+$sql = 'SELECT ID, Name FROM city WHERE Name = :name LIMIT 3';
+$pdoStatement = $pdoHandle->prepare($sql);
+$pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+$pdoStatement->execute(array('name' => 'Kabul'));
+print_r($pdoStatement->fetchAll());
+
+// SQL Statement Code w/ Injection
+$pdoHandle = CustomDB::getDBH();
+$_SERVER['urlParameter'] = "Kabul'; DROP DATABASE world;";
+$name = $_SERVER['urlParameter'];
+$sql = 'SELECT ID, Name FROM city WHERE Name = ' . $name . ' LIMIT 3';
+//$pdoStatement = $pdoHandle->query($sql);
+//$pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+//print_r($pdoStatement->fetchAll());
+
+// Prep Stmt Code w/ Injection
+$pdoHandle = CustomDB::getDBH();
+$_SERVER['urlParameter'] = "Kabul'; DROP DATABASE world;";
+$name = $_SERVER['urlParameter'];
+$sql = 'SELECT ID, Name FROM city WHERE Name = :name LIMIT 3';
+$pdoStatement = $pdoHandle->prepare($sql);
+$pdoStatement->setFetchMode(PDO::FETCH_ASSOC);
+$pdoStatement->execute(array('name' => $name));
+print_r($pdoStatement->fetchAll());
